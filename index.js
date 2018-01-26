c6fd32aadbc4b592699fe76cb32c635190ffedcd
Jacek Olszak <jacek.olszak@4finance.com>
2018-01-26 20:07:03 +0100

    Add boiler plate

---
+++ index.js
@@ -0,0 +1,27 @@
+module.exports = class StreamsMessageChannel {
+    constructor(inputStream, outputStream) {
+        console.log('yupi')
+    }
+
+    addListener(eventType, listener) {
+        
+    }
+
+    removeListener(eventType, listener) {
+
+    }
+
+    start() {
+
+    }
+
+    send(message) {
+
+    }
+
+    stop() {
+
+    }
+}
+
+console.log('Hello world')
